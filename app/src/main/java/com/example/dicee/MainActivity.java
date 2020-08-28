7b7f44ebc2ce43947ceacd8edcfffa62710ad0d3
Tarwin.ev <bres.anisg@gmail.com>
2020-08-28 03:02:35 -0700

    Dicee main activity Java

--- app/src/main/java/com/example/dicee/MainActivity.java
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.dicee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonRoll = findViewById(R.id.button_roll);
+        ImageView leftDice = findViewById(R.id.image_leftDice);
+        ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        buttonRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Dicee", "Button has been pressed!");
+            }
+        });
+
     }
 }
